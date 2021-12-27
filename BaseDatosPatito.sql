708d7f6865cf252c7f54f3710329be16ab242a42
Mariano Camposeco <mariano1941@outlook.es>
2021-12-27 01:30:03 -0600

    Actualizamos base de datos para examenes

--- BaseDatosPatito.sql
+++ BaseDatosPatito.sql
@@ -16,28 +16,20 @@ CREATE TABLE usuario(
     CONSTRAINT PK_USUARIO PRIMARY KEY(usuario),
     CONSTRAINT FK_TO_TURNO FOREIGN KEY(idTurno) REFERENCES turno(idTurno)
 	);
-CREATE TABLE subExamen(
-	tipo VARCHAR(25) NOT NULL,
-    nombre VARCHAR(25) NOT NULL,
-    CONSTRAINT PK_SUBEXAMENES PRIMARY KEY(tipo)
-	);
-CREATE TABLE muestra(
-	codigoMuestra INT NOT NULL,
-    nombreMuestra VARCHAR(25) NOT NULL,
-    fecha DATETIME NOT NULL,
-    CONSTRAINT PK_MUESTRA PRIMARY KEY(codigoMuestra)
-	);
 CREATE TABLE examen(
 	codigoExamen INT NOT NULL,
     nombreExamen VARCHAR(25) NOT NULL,
     precioExamen DOUBLE NOT NULL,
-    tipo VARCHAR(25) NOT NULL,
-    codigoMuestra INT NOT NULL,
-    CONSTRAINT PK_EXAMEN PRIMARY KEY(codigoExamen),
-    CONSTRAINT FK_TO_SUBEXAMEN FOREIGN KEY(tipo) REFERENCES subExamen(tipo),
-    CONSTRAINT FK_TO_MUESTRA FOREIGN KEY(codigoMuestra) REFERENCES muestra(codigoMuestra)
+    nombreMuestra VARCHAR(25) NOT NULL,
+    CONSTRAINT PK_EXAMEN PRIMARY KEY(codigoExamen)
 	);
-
+CREATE TABLE subExamen(
+	tipo VARCHAR(25) NOT NULL,
+    nombreSub VARCHAR(25) NOT NULL,
+    codigoExamen INT NOT NULL,
+    CONSTRAINT PK_SUBEXAMENES PRIMARY KEY(tipo,nombreSub),
+    CONSTRAINT FK_TO_EXAMEN FOREIGN KEY(codigoExamen) REFERENCES examen(codigoExamen)
+);
 CREATE TABLE regalias(
 	numeroColegiado CHAR(16) NOT NULL,
     medico VARCHAR(25) NOT NULL,
@@ -61,14 +53,10 @@ CREATE TABLE examenRealizar(
     fechaExamen DATE NOT NULL,
     resultados VARCHAR(45) NOT NULL,
     cui CHAR(14) NOT NULL,
-    codigoExamen INT NOT NULL,
     tipo VARCHAR(25) NOT NULL,
-    codigoMuestra INT NOT NULL,
     CONSTRAINT PK_EXAMENREALIZAR PRIMARY KEY(idExamen),
     CONSTRAINT FK_TO_PACIENTE FOREIGN KEY(cui) REFERENCES paciente(cui),
-	CONSTRAINT FK_TO_EXAMEN FOREIGN KEY(codigoExamen) REFERENCES examen(codigoExamen),
-    CONSTRAINT FK_TO_SUBEXAMEN2 FOREIGN KEY(tipo) REFERENCES subExamen(tipo),
-    CONSTRAINT FK_TO_MUESTRA2 FOREIGN KEY(codigoMuestra) REFERENCES muestra(codigoMuestra)
+    CONSTRAINT FK_TO_SUBEXAMEN FOREIGN KEY(tipo) REFERENCES subExamen(tipo)
 	);
 CREATE TABLE reporte(
 	codigoReporte INT NOT NULL,
@@ -77,14 +65,11 @@ CREATE TABLE reporte(
     cui CHAR(14) NOT NULL,
     codigoExamen INT NOT NULL,
     tipo VARCHAR(25) NOT NULL,
-    codigoMuestra INT NOT NULL,
     numeroColegiado CHAR(16),
     CONSTRAINT PK_REPORTE PRIMARY KEY(codigoReporte),
     CONSTRAINT FK_TO_EXAMENREALIZAR FOREIGN KEY(idExamen) REFERENCES examenRealizar(idExamen),
     CONSTRAINT FK_TO_PACIENTE2 FOREIGN KEY(cui) REFERENCES paciente(cui),
-	CONSTRAINT FK_TO_EXAMEN2 FOREIGN KEY(codigoExamen) REFERENCES examen(codigoExamen),
-    CONSTRAINT FK_TO_SUBEXAMEN3 FOREIGN KEY(tipo) REFERENCES subExamen(tipo),
-    CONSTRAINT FK_TO_MUESTRA3 FOREIGN KEY(codigoMuestra) REFERENCES muestra(codigoMuestra),
+    CONSTRAINT FK_TO_SUBEXAMEN2 FOREIGN KEY(tipo) REFERENCES subExamen(tipo),
     CONSTRAINT FK_TO_REGALIAS2 FOREIGN KEY(numeroColegiado) REFERENCES regalias(numeroColegiado)
 	);
 
@@ -95,12 +80,12 @@ CREATE TABLE inventario(
     descripcion VARCHAR(25),
     codigoReporte INT NOT NULL,
     cui CHAR(14) NOT NULL,
-    idExamen INT NOT NULL,
+    tipo VARCHAR(25) NOT NULL,
     numeroColegiado CHAR(16),
     CONSTRAINT PK_INVENTARIO PRIMARY KEY(idInventario),
     CONSTRAINT FK_TO_REPORTE FOREIGN KEY(codigoReporte) REFERENCES reporte(codigoReporte),
     CONSTRAINT FK_TO_PACIENTE3 FOREIGN KEY(cui) REFERENCES paciente(cui),
-    CONSTRAINT FK_TO_EXAMEN3 FOREIGN KEY(idExamen) REFERENCES examenRealizar(idExamen),
+    CONSTRAINT FK_TO_SUBEXAMEN3 FOREIGN KEY(tipo) REFERENCES subExamen(tipo),
     CONSTRAINT FK_TO_REGALIAS3 FOREIGN KEY(numeroColegiado) REFERENCES regalias(numeroColegiado)
 	);
 CREATE TABLE corteMes(
@@ -111,13 +96,13 @@ CREATE TABLE corteMes(
     idInventario INT NOT NULL,
     codigoReporte INT NOT NULL,
     cui CHAR(14) NOT NULL,
-    idExamen INT NOT NULL,
+    tipo VARCHAR(25) NOT NULL,
     numeroColegiado CHAR(16),
     CONSTRAINT PK_CORTEMES PRIMARY KEY(fecha),
     CONSTRAINT FK_TO_INVENTARIO FOREIGN KEY(idInventario) REFERENCES inventario(idInventario),
     CONSTRAINT FK_TO_REPORTE1 FOREIGN KEY(codigoReporte) REFERENCES reporte(codigoReporte),
     CONSTRAINT FK_TO_PACIENTE4 FOREIGN KEY(cui) REFERENCES paciente(cui),
-    CONSTRAINT FK_TO_EXAMEN4 FOREIGN KEY(idExamen) REFERENCES examenRealizar(idExamen),
+    CONSTRAINT FK_TO_SUBEXAMEN4 FOREIGN KEY(tipo) REFERENCES subExamen(tipo),
     CONSTRAINT FK_TO_REGALIAS4 FOREIGN KEY(numeroColegiado) REFERENCES regalias(numeroColegiado)
 	);
     INSERT INTO turno(idTurno, area, horarioIngreso, dias) VALUES
@@ -126,3 +111,30 @@ CREATE TABLE corteMes(
 ('Juana', 'juana@gmail.com','$2a$10$bTXb/uRl5aFF5nxHtlD04.Q6YaoanfQMrySIRD4yIpFo.o7SrsqHW', 2 ,1),
 ('Marco', 'marco@gmail.com','$2a$10$bTXb/uRl5aFF5nxHtlD04.Q6YaoanfQMrySIRD4yIpFo.o7SrsqHW',1 ,1),
 ('Ale', 'ale@gmail.com','$2a$10$bTXb/uRl5aFF5nxHtlD04.Q6YaoanfQMrySIRD4yIpFo.o7SrsqHW',3 ,1);
+INSERT INTO examen(codigoExamen, nombreExamen, precioExamen, nombreMuestra) VALUES
+(100, 'Hematologia',1200, 'Sangre'),
+(101, 'Orina',800, 'Orina'),
+(102, 'Heces',600, 'Heces')
+;
+INSERT INTO subExamen(tipo, nombreSub,codigoExamen) VALUES
+('Completo', 'Globulos Rojos',100),
+('Completo', 'Hemoglobina',100),
+('Completo', 'Hematocrito',100),
+('Completo', 'Basofilos',100),
+('Macroscopico', 'Densidad especifica',101),
+('Macroscopico', 'Aspecto',101),
+('Macroscopico', 'Olor',101),
+('Macroscopico', 'Color',101),
+('Macroscopico', 'Restos alimenticios',102),
+('Macroscopico', 'Consistencia',102),
+('Macroscopico', 'Sangre',102),
+('Macroscopico', 'Reaccion pH',102),
+('Microscopico', 'Bacterias',101),
+('Microscopico', 'Hemoglobina',101),
+('Microscopico', 'Bilirrubina',101),
+('Microscopico', 'Nitritos',101),
+('Microscopico', 'Leucocitos',102),
+('Microscopico', 'Eritrocitos',102),
+('Microscopico', 'Grasas',102),
+('Microscopico', 'Jabones',102)
+;
